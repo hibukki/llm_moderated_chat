8864c2a187b57b472c3e39d99d8b71d3cea33766
Yonatan Cale <myselfandfredy@gmail.com>
2025-05-04 18:32:46 +0300

    feat: initial project setup with vite, react, ts, eslint, vitest, husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime' // Needed for new JSX transform
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } }, // Automatically detect React version
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }], // Warn on unused vars, ignore if starts with _
+    'react/prop-types': 'off', // Disable prop-types rule, using TypeScript
+  },
+}; 
